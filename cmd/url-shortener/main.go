2c87b344fabb966a1492984b5df0549536db4417
Александр Клинный <klinnyy.a.i@edu.mirea.ru>
2026-01-25 16:49:28 +0300

    first commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the URL Shortener Service!")
+	//TODO: Init Config
+
+
+
+	//TODO: Init Logger log/slog
+
+	//TODO: Init storage sqlite
+
+	//TODO: Init Router
+
+	//TODO: Start Server
+}
